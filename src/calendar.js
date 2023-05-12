5788c7cb2d41cb28d0050436192cb283071e1764
Gourab Porey <gourabporey@users.noreply.github.com>
2023-05-12 13:09:49 +0530

    Fixed bug in the labels of the month names

--- src/calendar.js
+++ src/calendar.js
@@ -13,17 +13,9 @@ class Calendar {
     this.#days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
   }
 
-  #getDaysInMonth(monthIndex, year = 2023) {
-    return new Date(year, monthIndex + 1).getUTCDate();
-  }
-
-  #getStartingDay(monthIndex, year = 2023) {
-    return new Date(year, monthIndex, 1).getDay();
-  }
-
   getMonthlyCalendar(monthIndex, year = 2023) {
-    const daysInMonth = this.#getDaysInMonth(monthIndex, year);
-    const startingDay = this.#getStartingDay(monthIndex, year);
+    const daysInMonth = new Date(year, monthIndex + 1).getUTCDate();
+    const startingDay = new Date(year, monthIndex, 1).getDay();
 
     return Array.from({ length: 35 }, function (_, i) {
       const date = i - startingDay + 1;
@@ -32,9 +24,7 @@ class Calendar {
   }
 
   #renderWeek(week) {
-    return week.map(function (day) {
-      return centerAlign(day, 2);
-    }).join(" ");
+    return week.map((day) => { return centerAlign(day, 2); }).join(" ");
   }
 
   monthToString(monthlyCalendar, monthIndex, year = 2023) {
@@ -53,20 +43,31 @@ class Calendar {
     });
   }
 
-  #quarterToString(quarter, quarterIndex) {
-    const first = this.#months[quarterIndex];
-    const second = this.#months[quarterIndex + 1];
-    const third = this.#months[quarterIndex + 2];
+  #getHeadline([a, b, c]) {
+    return centerAlign(a, 20) + centerAlign(b, 28) + centerAlign(c, 20);
+  }
 
-    const heading = centerAlign(first, 20) + centerAlign(second, 28) + centerAlign(third, 20);
+  #generateRowOfQuarters(weeks, i) {
+    return [...weeks[i], "  ", ...weeks[i + 5], "  ", ...weeks[i + 10]];
+  }
 
-    const weeklyCalendars = quarter.flatMap(function (month) {
-      return chunk(month, 7);
+  #generateQuarterlyDayLabels() {
+    return new Array(5).fill().flatMap((_, i) => {
+      return i % 2 === 0 ? this.#days : " ";
     });
+  }
 
-    const rows = [[heading]];
+  #quarterToString(quarter, quarterIndex) {
+    const firstMonthIndex = quarterIndex * 3;
+    const months = this.#months.slice(firstMonthIndex, firstMonthIndex + 3);
+    const weeks = quarter.flatMap((month) => { return chunk(month, 7); });
+
+    const heading = this.#getHeadline(months);
+    const labels = this.#generateQuarterlyDayLabels();
+    const rows = [[heading], labels];
+
     for (let i = 0; i < 5; i++) {
-      rows.push([...weeklyCalendars[i], "  ", ...weeklyCalendars[i + 5], "  ", ...weeklyCalendars[i + 10]]);
+      rows.push(this.#generateRowOfQuarters(weeks, i));
     }
 
     return rows.map(this.#renderWeek).join("\n");
@@ -75,11 +76,12 @@ class Calendar {
   yearToString(yearlyCalendar, year) {
     const quarterlyCalendar = chunk(yearlyCalendar, 3);
 
-    const cal = this;
-    return quarterlyCalendar.reduce(function (eachQuarterText, quarter, quarterIndex) {
-      return `${eachQuarterText}${cal.#quarterToString(quarter, quarterIndex)}\n`;
+    return quarterlyCalendar.reduce((previousQuarterText, quarter, quarterIndex) => {
+      return `${previousQuarterText}${this.#quarterToString(quarter, quarterIndex)}\n\n`;
     }, "");
   }
 }
 
+const c = new Calendar();
+console.log(c.yearToString(c.getYearlyCalendar(2023), 2023));
 exports.Calendar = Calendar;
